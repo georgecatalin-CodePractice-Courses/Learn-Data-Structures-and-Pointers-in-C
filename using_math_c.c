cb217611da5b358f89e7b52118c4d30db5af91ae
George Calin <george.calin@gmail.com>
2020-04-08 17:40:27 +0300

    Solving math puzzles

---
+++ using_math_c.c
@@ -0,0 +1,18 @@
+/* *****************************
+* Using Math()
+* **************************** */
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a;
+
+    for (a = 0.0  ; a < 20.0; a+=1.0)
+    {
+        printf("%.0f, \n", pow(2.0,a));
+    }
+    
+    return (0);
+}
